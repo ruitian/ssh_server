52da0f7a54e569d9e28ca837ccad7918056ca913
baron <ike960705@gmail.com>
2016-04-27 11:13:49 +0800

    modify

--- manage.py
+++ manage.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from ssh import app
+from app import app
 from views import *  # noqa
 
 if __name__ == '__main__':
--- views.py
+++ views.py
@@ -1,8 +1,17 @@
 # -*- coding: utf-8 -*-
 from flask import render_template
+from flask import request
 
-from ssh import app
+from app import app
+from ssh import Connect
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        con = Connect()
+        name = request.form['name']
+        host = request.form['host']
+        passwd = request.form['passwd']
+        cmd = request.form['cmd']
+        return con.get_connect(name, host, passwd, cmd)
     return render_template('index.html')
