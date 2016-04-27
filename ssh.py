69b001f9c9c845ff1162389b3a9d76f077954266
baron <ike960705@gmail.com>
2016-04-27 11:14:21 +0800

    create connect function

--- ssh.py
+++ ssh.py
@@ -1,4 +1,32 @@
 # -*- coding: utf-8 -*-
-from flask import Flask
+import pexpect
 
-app = Flask(__name__)
+
+class Connect():
+
+    def get_connect(self, name, host, passwd, cmd):
+        cmd = 'ssh -t ' + name + '@' + host + ' ' + '"' + cmd + '"'
+        ssh = pexpect.spawn(cmd)
+        try:
+            flag = ssh.expect(['password:', 'continue connecting (yes/no)?'], timeout = 10)
+            if flag == 0:
+                ssh.sendline(passwd)
+                return ssh.read()
+
+            elif flag == 1:
+                ssh.sendline("yes")
+                flag = 0
+
+            return {
+                'flag': flag
+            }
+        except pexpect.EOF:
+            print "EOF"
+            ssh.close()
+            ret = -1
+        except pexpect.TIMEOUT:
+            print "TIMEOUT"
+            ssh.close()
+            ret = -2
+
+        return ret
