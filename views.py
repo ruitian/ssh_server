6fc81ff8fc65e97f3a7af15dc2c427a115ee2e27
baron <ike960705@gmail.com>
2016-04-27 22:10:36 +0800

    add sessionn

--- views.py
+++ views.py
@@ -1,18 +1,41 @@
 # -*- coding: utf-8 -*-
 from flask import render_template
+from flask import redirect
 from flask import request
+from flask import session
+from flask import url_for
 
 from app import app
 from ssh import Connect
 
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    if request.method == 'POST':
-        con = Connect()
-        name = request.form['name']
-        host = request.form['host']
-        passwd = request.form['passwd']
-        cmd = request.form['cmd']
-        result = con.send_cmd(name, host, passwd, cmd)
-        return render_template('index.html', result=result)
     return render_template('index.html')
+
+
+@app.route('/result')
+def result():
+    return session['name']
+
+
+@app.route('/connect', methods=['GET', 'POST'])
+def connect():
+    session['name'] = name = request.form['name']
+    session['host'] = host = request.form['host']
+    session['passwd'] = passwd = request.form['passwd']
+    con = Connect()
+    print con.get_connect(name, host, passwd)
+    if con.get_connect(name, host, passwd) == 0:
+        return redirect(url_for('index'))
+    else:
+        return redirect(url_for('deactive'))
+
+
+@app.route('/deactive', methods=['POST', 'GET'])
+def deactive():
+    if request.method == 'POST' or request.method == 'GET':
+        session.pop('name')
+        session.pop('host')
+        session.pop('passwd')
+        return redirect(url_for('index'))
