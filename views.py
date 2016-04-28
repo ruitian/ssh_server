3832bac88245f4363a6289c24a4227a2448c1930
baron <ike960705@gmail.com>
2016-04-28 12:42:33 +0800

    timeout

--- views.py
+++ views.py
@@ -4,6 +4,7 @@ from flask import redirect
 from flask import request
 from flask import session
 from flask import url_for
+from flask import flash
 
 from app import app
 from ssh import Connect
@@ -14,22 +15,21 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/result')
-def result():
-    return session['name']
-
-
 @app.route('/connect', methods=['GET', 'POST'])
 def connect():
     session['name'] = name = request.form['name']
     session['host'] = host = request.form['host']
     session['passwd'] = passwd = request.form['passwd']
     con = Connect()
-    print con.get_connect(name, host, passwd)
-    if con.get_connect(name, host, passwd) == 0:
-        return redirect(url_for('index'))
-    else:
+    flag = con.get_connect(name, host, passwd)
+    if flag == 0:
+        flash(u'连接失败，请检查您的输入', 'danger')
         return redirect(url_for('deactive'))
+    elif flag == -2 or flag == -1:
+        flash(u'连接超时，请尝试重新连接', 'danger')
+        return redirect(url_for('deactive'))
+    else:
+        return redirect(url_for('index'))
 
 
 @app.route('/deactive', methods=['POST', 'GET'])
@@ -39,3 +39,12 @@ def deactive():
         session.pop('host')
         session.pop('passwd')
         return redirect(url_for('index'))
+
+
+@app.route('/<command>')
+def send_cmd(command):
+    pass
+
+
+def send_and_get(command):
+    pass
